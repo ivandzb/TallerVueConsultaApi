976db57aa307dc3f1b0908740a4772321b49f5ce
ivandzb <ivandzb@gmail.com>
2019-01-31 07:50:09 -0500

    Dia 1
    Conectandome a la aopi

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img width="25%" src="./assets/logo.png">
-    <HelloWorld/>
+    <h1 class="title">Consultar datos</h1>
+    <button v-on:click="fetch">Consultar</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import axios from "axios";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  data: function(){
+    return {
+      characters []
+    };
+
+  },
+  methods: {
+    fetch() {
+      let result = axios
+      .get("https://rickandmortyapi.com/api/character")
+      .then(res => {
+        this.characters = res.data.result;
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
