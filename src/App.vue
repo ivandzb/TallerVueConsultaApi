b369177ecdf440a9aa47367bebcb4413945d870f
ivandzb <ivandzb@gmail.com>
2019-02-01 13:49:15 -0500

    dia 3
    ya veo las imagenes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,44 @@
 <template>
   <div id="app">
+    <div class="hero is-white is-gradient is-bold">
+      <div class="hero-body">
+        <h1 class="title">
+          <span class="has-text-success">R&M</span>
+          <span class="subtitle">Personaje</span>
+        </h1>
+        <button class="button is-success is-rounded" v-on:click="fetch">
+          Consultar
+        </button>
+      </div>
+    </div>
+    <div class="container">
+      <div
+        class="columns is-desktop is-mobile is-tablet is-multiline is-centered"
+      >
+        <div
+          class="column is-12-meobile is-4-desktop is-4-tablet"
+          v-for="character of characters"
+          v-bind:key="character.id"
+        >
+          <div class="card">
+            <div class="card-header">
+              <img v-bind:src="character.image" v-bind:alt="character.name" />
+            </div>
+            <div class="card-content">
+              <h3 class="title is-size-4">{{ character.name }}</h3>
+              <button class="button title is-success is-rounded is-small">
+                Ver más
+              </button>
+
+              <div class="h3"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <h1 class="title">Consultar datos</h1>
-    <button v-on:click="fetch">Consultar</button>
+
     <div v-for="character of characters" v-bind:key="character.id">
       {{ character }}
     </div>
